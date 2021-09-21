92c7936388f47b210beb221dca0766c9deccb6d9
Norio Nomura <norio.nomura@gmail.com>
2021-09-21 18:20:05 +0900

    Fix mention ends with codeblock

--- src/utils/message.ts
+++ src/utils/message.ts
@@ -28,9 +28,9 @@ export function commandlines(message?: DiscordenoMessage): string[] | undefined 
 }
 
 export function commandlinesFrom(content: string, botId: bigint): string[] | undefined {
-  const patternForMentionToBot = new RegExp(`<@!?${botId}>(?<args>.*)$`, "mg");
+  const patternForMentionToBot = new RegExp(`<@!?${botId}>(?<args>.*?)(?:\`\`\`|$)`, "mg");
   const matches = content.matchAll(patternForMentionToBot);
-  const extractMention = (m: RegExpMatchArray) => m[0].replace(/<@!?\d+>/g, "");
+  const extractMention = (m: RegExpMatchArray) => m[1].replace(/<@!?\d+>/g, "");
   const lines = [...new Set([...matches].map(extractMention))];
   return lines.length != 0 ? lines : undefined;
 }

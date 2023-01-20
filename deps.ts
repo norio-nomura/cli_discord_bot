a3f20b54a17d65d2f34b9443c9ec361bcc2b3766
Norio Nomura <norio.nomura@gmail.com>
2023-01-20 16:58:13 +0900

    Update to std@0.165.0

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { assertEquals, assertThrows, fail } from "https://deno.land/std@0.107.0/testing/asserts.ts";
+export { assertEquals, assertThrows, fail } from "https://deno.land/std@0.165.0/testing/asserts.ts";
 export * from "https://raw.githubusercontent.com/norio-nomura/discordeno/patched-for-cli_discord_bot/mod.ts";

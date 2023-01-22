9bb7c739c44f6866721266cdffefdbdd2959e555
Norio Nomura <norio.nomura@gmail.com>
2023-01-22 16:42:44 +0900

    Add SIG{TERM,INT} handler

--- mod.ts
+++ mod.ts
@@ -1,7 +1,18 @@
-import { startBot } from "./deps.ts";
+import { startBot, stopBot } from "./deps.ts";
 import { configuration, Options, setOptions } from "./bot/mod.ts";
 
 export function startCLIBot(options: Partial<Options>) {
   setOptions(options);
-  startBot(configuration.bot);
+  const bot = configuration.bot;
+  Deno.addSignalListener("SIGINT", async () => {
+    console.info("Terminating by SIGINT");
+    await stopBot(bot);
+    Deno.exit();
+  });
+  Deno.addSignalListener("SIGTERM", async () => {
+    console.info("Terminating by SIGTERM");
+    await stopBot(bot);
+    Deno.exit();
+  });
+  startBot(bot);
 }
